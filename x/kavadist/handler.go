e570107fc315091ec4e38f5adfdc21ababa3df24
karzak <kjydavis3@gmail.com>
2021-06-02 11:24:59 -0600

    feat: add upgrade time to proposal hanlder

--- x/kavadist/handler.go
+++ x/kavadist/handler.go
@@ -1,12 +1,16 @@
 package kavadist
 
 import (
+	"time"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/kava-labs/kava/x/kavadist/keeper"
 	"github.com/kava-labs/kava/x/kavadist/types"
 )
+
+var v0142UpgradeTime = time.Date(2021, 6, 10, 14, 0, 0, 0, time.UTC)
 
 // NewHandler creates an sdk.Handler for kavadist messages
 func NewHandler(k Keeper) sdk.Handler {
@@ -23,8 +27,10 @@ func NewCommunityPoolMultiSpendProposalHandler(k Keeper) govtypes.Handler {
 	return func(ctx sdk.Context, content govtypes.Content) error {
 		switch c := content.(type) {
 		case types.CommunityPoolMultiSpendProposal:
-			return keeper.HandleCommunityPoolMultiSpendProposal(ctx, k, c)
-
+			if ctx.BlockTime().After(v0142UpgradeTime) {
+				return keeper.HandleCommunityPoolMultiSpendProposal(ctx, k, c)
+			}
+			return sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unrecognized kavadist proposal content type: %T", c)
 		default:
 			return sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unrecognized kavadist proposal content type: %T", c)
 		}
